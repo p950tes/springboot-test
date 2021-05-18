319be9604de5739570b133ebadb34945d1820672
p950tes <tobias.ericsson@hotmail.com>
2021-05-18 17:21:36 +0200

    Initial commit

---
+++ src/main/java/se/p950tes/springboottest/SpringbootTestApplication.java
@@ -0,0 +1,13 @@
+package se.p950tes.springboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/p950tes/springboottest/SpringbootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package se.p950tes.springboottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
