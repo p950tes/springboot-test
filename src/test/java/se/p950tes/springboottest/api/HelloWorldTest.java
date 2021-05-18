afad9e35a49c3cf686828f7fb1760c6a7a85ce7f
p950tes <tobias.ericsson@hotmail.com>
2021-05-18 18:28:36 +0200

    Added failing test again

--- src/test/java/se/p950tes/springboottest/api/HelloWorldTest.java
+++ src/test/java/se/p950tes/springboottest/api/HelloWorldTest.java
@@ -13,6 +13,6 @@ public class HelloWorldTest {
 	@Test
 	public void test() throws Exception {
 		HelloWorldResponse response = controller.helloWorld();
-		assertEquals("Goodbye World", response.getResponse());
+		assertEquals("Goodbye World2", response.getResponse());
 	}
 }
